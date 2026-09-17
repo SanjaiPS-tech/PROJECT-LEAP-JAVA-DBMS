216d1a68f613f4a45e791ec520233ad5ece4a1b1
SanjaiPS-tech <sanjaips.tech@gmail.com>
2026-09-17 12:51:00 +0530

    day 2 for java & dbms leap connection with mysql

--- jdbc/src/main/java/com/project/Main.java
+++ jdbc/src/main/java/com/project/Main.java
@@ -1,7 +1,22 @@
 package com.project;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String url = "jdbc:mariadb://localhost:3306/";
+        String user = "developer";
+        String password = "123";
+        String query = "select * from user";
+
+        Connection connection = DriverManager.getConnection(url, user, password);
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ResultSet ResultSet = preparedStatement.executeQuery(query);
+
+        while (ResultSet.next()) {
+            System.out.println(ResultSet.getString("name"));
+        }
     }
 }
